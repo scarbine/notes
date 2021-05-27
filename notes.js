b2247e9a7ff26f51877b2bcc5699ee013d1ba26d
Sam Carbine <scarbine@me.com>
2021-05-27 15:54:59 -0500

    first commit

---
+++ notes.js
@@ -0,0 +1,19 @@
+
+const notes = [
+    {
+        id: 1 ,
+        subject: "I learned today",
+        date: "12/24/12",
+        feeling: "mood",
+        timeSpent: 23
+    },
+    {
+        id: 1 ,
+        subject: "I learned today",
+        date: "12/24/12",
+        feeling: "mood",
+        timeSpent: 23
+    }
+]
+
+console.log(notes)
