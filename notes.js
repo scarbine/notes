c91814992f9b4407116c7b16b16c496cce22403f
Sam Carbine <scarbine@me.com>
2021-05-27 16:15:05 -0500

    add new note to the array via push

--- notes.js
+++ notes.js
@@ -1,19 +1,36 @@
-
+// This is the array of objects(notes) that contains all the notes recorded
 const notes = [
     {
         id: 1 ,
         subject: "I learned today",
-        date: "12/24/12",
+        date: "5-24-21",
         feeling: "mood",
         timeSpent: 23
     },
     {
-        id: 1 ,
+        id: 2 ,
         subject: "I learned today",
-        date: "12/24/12",
+        date: "5-25-21",
         feeling: "mood",
         timeSpent: 23
     }
 ]
 
-console.log(notes)
+console.log(notes) ;
+
+const noteAboutToday = {
+    
+    id: 3 ,
+    subject: "Learning more ",
+    date: "5-26-21",
+    feeling: "mood",
+    timeSpent: 23 
+}
+// this is adding the noteAboutToday to the notes array
+
+notes.push(noteAboutToday) ;
+
+// This logs out the new array
+
+console.log(notes) ;
+
